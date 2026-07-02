4cd94318b67fd50a94db8bf98639963ee7223688
Rishiraj Tamuly <107537483+rishirajtamuly@users.noreply.github.com>
2026-07-02 21:04:17 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,22 +1,12 @@
-console.log("NEW SCRIPT LOADED");
-
 const form = document.getElementById("contactForm");
 
 form.addEventListener("submit", async function (e) {
-
-    console.log("1. Submit clicked");
-
     e.preventDefault();
 
     const formData = new FormData(form);
     const data = Object.fromEntries(formData);
 
-    console.log("2. Data =", data);
-
     try {
-
-        console.log("3. Sending fetch...");
-
         const response = await fetch("/contact", {
             method: "POST",
             headers: {
@@ -25,18 +15,10 @@ form.addEventListener("submit", async function (e) {
             body: JSON.stringify(data)
         });
 
-        console.log("4. Response status:", response.status);
-
         const result = await response.text();
-
-        console.log("5. Server says:", result);
-
         alert(result);
 
     } catch (err) {
-
         console.error("FETCH ERROR:", err);
-
     }
-
 });
